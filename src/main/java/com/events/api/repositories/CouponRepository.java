f3624dfecb6c851c9d8cf877d1d4a4c9db083f84
FlavioMontoril <flavio.montorilll@gmail.com>
2026-04-24 22:10:00 -0300

    implementes function to find cupon by id and with event

--- src/main/java/com/events/api/repositories/CouponRepository.java
+++ src/main/java/com/events/api/repositories/CouponRepository.java
@@ -1,11 +1,18 @@
 package com.events.api.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.events.api.domain.Coupon;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+
+    // Query 2: Busca customizada para trazer o evento junto em um único JOIN
+    @Query("SELECT c FROM Coupon c JOIN FETCH c.event WHERE c.id = :id")
+    Optional<Coupon> findByIdWithEvent(@Param("id") UUID id);
 
 }
